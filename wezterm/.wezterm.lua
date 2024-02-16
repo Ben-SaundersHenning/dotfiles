69a93c2781373c54fc3d0689b59680838a045bd6
Ben Saunders-Henning <bsaund03@uoguelph.ca>
2024-02-16 10:46:54 -0500

    Added platform specific lines for Wezterm fonts.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,7 +11,8 @@ config.color_scheme = 'GruvboxDark'
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font = wezterm.font("Hasklug Nerd Font", { weight="Regular", stretch="Normal", style="Normal" })
+config.font = wezterm.font("Hasklug Nerd Font", { weight="Regular", stretch="Normal", style="Normal" }) -- FOR SUSE
+-- config.font = wezterm.font("Iosevka Nerd Font", { weight="Regular", stretch="Normal", style="Normal" }) -- FOR MAC
 config.font_size = 14.0
 
 return config
