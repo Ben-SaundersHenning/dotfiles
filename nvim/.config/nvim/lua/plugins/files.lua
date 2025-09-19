848a7336bf8966ae05514b7e2474d935f22cb8aa
Ben Saunders-Henning <ben.shp@proton.me>
2025-09-18 22:04:23 -0400

    Add yazi.nvim

--- nvim/.config/nvim/lua/plugins/files.lua
+++ nvim/.config/nvim/lua/plugins/files.lua
@@ -3,11 +3,41 @@ return {
   -- Mini file manager
   {
 
-    'nvim-mini/mini.files',
+    "nvim-mini/mini.files",
 
     version = false,
     opts = {}
 
   },
+
+  ---@type LazySpec
+  {
+
+    "mikavilpas/yazi.nvim",
+
+    version = "*", -- use the latest stable version
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>r",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+
+  }
 
 }
