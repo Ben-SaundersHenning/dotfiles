a98a13f1218f8933f31676ca767c2bb56bc74fd4
Ben Saunders-Henning <ben.shp@proton.me>
2025-09-25 12:26:56 -0400

    Move yazi shortcut to global

--- zsh/.zsh/tumbleweed.zsh
+++ zsh/.zsh/tumbleweed.zsh
@@ -36,10 +36,3 @@ function newnote() {
     typst init @local/notes:0.0.1
 }
 
-function y() {
-    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-    yazi "$@" --cwd-file="$tmp"
-    IFS= read -r -d '' cwd < "$tmp"
-    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-    rm -f -- "$tmp"
-}
