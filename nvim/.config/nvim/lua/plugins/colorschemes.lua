a0471a775e62b6091b2abf3bd28ce346dbe76bdd
Ben Saunders-Henning <ben.shp@proton.me>
2025-09-29 20:54:22 -0400

    Add snacks

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,13 +1,23 @@
 return {
 
-    --gruvbox by ellisonleo {colorscheme}
-    {
+  -- treesitter gruvbox
+  {
 
-	"ellisonleao/gruvbox.nvim", 
-	priority = 1000, 
-	config = true, 
-	opts = {}
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    opts = {}
 
-    },
+  },
+
+  -- --gruvbox by ellisonleo {colorscheme}
+  -- {
+  --
+  --   "ellisonleao/gruvbox.nvim", 
+  --   priority = 1000, 
+  --   config = true, 
+  --   opts = {}
+  --
+  -- },
 
 }
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+
+  {
+
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      scroll = {
+        enabled = true,
+      },
+      indent = {
+        enabled = true,
+      },
+      animate = {
+        enabled = true,
+      },
+    }
+  }
+
+}
