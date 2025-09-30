8c7536263788cfd839bb6f9cd67c7a6c1dc1ddf7
Ben Saunders-Henning <ben.shp@proton.me>
2025-09-30 12:00:18 -0400

    .

--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -5,3 +5,8 @@ opt.shiftwidth = 2
 
 -- Make tabs count as 2 spaces (default is 8)
 opt.tabstop = 2
+
+opt.textwidth = 100
+opt.wrapmargin = 0
+opt.formatoptions:append { 't' }
+opt.linebreak = true
