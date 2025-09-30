1aac2659ba8f67be2a058e1035da7397a556c9f2
Ben Saunders-Henning <ben.shp@proton.me>
2025-09-29 20:54:22 -0400

    Add snacks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,5 +23,6 @@ require('config.mappings')
 require('config.lazy')
 
 vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
+vim.g.gruvbox_material_enable_italic = true
+vim.cmd('colorscheme gruvbox-material')
 
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -45,6 +45,17 @@ return {
       theme = 'jellybeans'
     },
     -- extensions = {'trouble'}
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   }
 
 }
